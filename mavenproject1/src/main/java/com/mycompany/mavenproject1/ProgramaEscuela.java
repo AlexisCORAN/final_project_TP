ca66703b05c717d0d352ac2b30121a515b63ffc1
Alexis Coronado <77992523+AlexisCORAN@users.noreply.github.com>
2025-11-22 16:59:20 -0500

    Refactor ProgramaEscuela class

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/ProgramaEscuela.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/ProgramaEscuela.java
@@ -1,30 +1,29 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.mavenproject1;
 
+package com.mycompany.programaescuela;
+import java.io.IOException;
 import java.util.Scanner;
-
 /**
  *
- * @author tiago
+ * @author Alexis
  */
 public class ProgramaEscuela {
-     public static void main(String[] args) {
+
+    public static void main(String[] args) {
        SistemaNotas sistema = new SistemaNotas();
        Scanner tecla = new Scanner(System.in);
         
         
         String archivoEntrada = "base_de_datos2.csv";
-        String archivoSalida = "Reporte_Final.csv";
         
         int opcion = 0;
 
         do {
             System.out.println("\n=== SISTEMA DE CONTROL DE NOTAS ===");
             System.out.println("1. Cargar Datos (Excel/CSV)");
-            System.out.println("2. Mostrar Reporte en Pantalla");
+            System.out.println("2. Mostrar Reporte de promedios");
             System.out.println("3. Exportar Resultados");
             System.out.println("4. Salir");
             System.out.print("Seleccione una opcion: ");
@@ -35,15 +34,22 @@ public class ProgramaEscuela {
 
             switch (opcion) {
                 case 1:
-                    if (sistema.cargarDatosDesdeArchivo(archivoEntrada)) {
-                        System.out.println("Carga exitosa");
+                {
+                    try {
+                        if (sistema.cargaDatos(archivoEntrada)) {
+                            System.out.println("Carga exitosa");
+                        }
+                    } catch (IOException ex) {
+                        System.getLogger(ProgramaEscuela.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
                     }
+                            
+                }
                     break;
+
                 case 2:
-                    sistema.calcularPromedio();
+                    sistema.listarPromedios();
                     break;
                 case 3:
-                    sistema.exportarReporte(archivoSalida);
                     break;
                 case 4:
                     System.out.println("Cerrando sistema");
@@ -52,8 +58,6 @@ public class ProgramaEscuela {
                     System.out.println("Opción no válida.");
             }
 
-        } while (opcion != 4);
-    }
-}
-
+        } while (opcion != 4);
+    }
 }
